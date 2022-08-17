abe2b7cf4109ad5e4e2681134b36dc697c76891c
victordias <victoroliveirad96@gmail.com>
2022-08-17 20:13:22 -0300

    chore(eslint): install and configure eslint and path mapping

---
+++ App.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Text } from "react-native";
+
+export default function App() {
+  return <Text>Open up App.js to start working on your app!</Text>;
+}
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,27 @@
-module.exports = function(api) {
+
+export default function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ['babel-preset-expo'],
+    // below, configure 'module-resolver' plugin and path mappings
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          extensions: [
+            '.ts',
+            '.tsx',
+            '.js',
+            '.json'
+          ],
+          alias: {
+            '@components': './src/components',
+            '@screens': './src/screens',
+            '@assets': './src/assets'
+          }
+        }
+      ]
+    ]
   };
-};
+}
